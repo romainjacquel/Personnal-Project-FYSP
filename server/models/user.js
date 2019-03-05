90c84e2d48d124f576950b09c771f07bc054c5c1
Romain JACQUEL <romainjacquel3@gmail.com>
2019-03-05 14:43:45 +0000

    Cryptage du password

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs')
 
 // Création d'un model
 
@@ -8,6 +9,21 @@ const userSchema = new Schema({
     password : String
 })
 
+userSchema.pre("save",function(next){
+    const user = this;
+    bcrypt.genSalt(10, function(err,salt){
+        if(err){
+            return next(err)
+        }
+        bcrypt.hash(user.password,salt,null,function(err,hash){
+            if(err){
+               return next(err)
+            }
+            user.password = hash;
+            next();
+        })
+    })
+})
 const UserModel = mongoose.model('user', userSchema);
 
 module.exports = UserModel
